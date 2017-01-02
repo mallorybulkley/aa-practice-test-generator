7f7f30790b86223253f5b8e8804b53968ea9651a
Mallory Bulkley <mallorybulkley@Mallorys-MacBook-Pro.local>
2017-01-01 22:28:06 -0500

    Remove unnecessary block spec from Binary Search problem

--- problems/binary_search.rb
+++ problems/binary_search.rb
@@ -1,7 +1,7 @@
 class Array
 
-  # Write a monkey patch of binary search that accepts a comparison block:
-  # E.g. [1, 2, 3, 4, 5, 7].my_bsearch(6) {|el, targ| el+1 <=> targ} => 4
+  # Write a monkey patch of binary search:
+  # E.g. [1, 2, 3, 4, 5, 7].my_bsearch(5) => 4
   def my_bsearch(target, &prc)
 
   end
