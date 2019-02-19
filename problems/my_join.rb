29f7238783230d72201251a1a154a3cc8da92e5a
Simcha Cohen <simcha_cohen@yahoo.com>
2019-02-19 11:07:21 -0500

    my_join

--- problems/my_join.rb
+++ problems/my_join.rb
@@ -1,5 +1,12 @@
 class Array
 
+  # Monkey patch the Array class and add a my_join method. If my_join receives
+  # no argument, then use an empty string to join the array together.
+  # ex.
+  # [1,2,3].my_join => '123'
+  # [1,2,3].my_join('$') => '1$2$3'
+
+
   def my_join(str = "")
 
   end
--- solutions/my_join_solution.rb
+++ solutions/my_join_solution.rb
@@ -1,13 +1,14 @@
 class Array
 
   def my_join(str = "")
-    res = ""
-    my_each_with_index do |el, i|
-      res << el.to_s
-      res << str unless i == size - 1
+    new_str = ""
+    each_with_index do |el, i|
+      new_str << el.to_s
+      new_str << str unless i == length - 1 
+      # don't want to add the str to the end of the new_str
     end
 
-    res
+    new_str
   end
 
 end
