c09db805ee203ce470d560897a29c143bba10ace
Simcha Cohen <simcha_cohen@yahoo.com>
2019-02-19 16:45:32 -0500

    my_rotate

--- solutions/my_rotate_solution.rb
+++ solutions/my_rotate_solution.rb
@@ -1,9 +1,9 @@
 class Array
   
-    def my_rotate(positions = 1)
-      split_idx = positions % self.length
-  
-      self.drop(split_idx) + self.take(split_idx)
-    end
+  def my_rotate(positions = 1)
+    split_idx = positions % self.length
+
+    self.drop(split_idx) + self.take(split_idx)
+  end
 
 end
