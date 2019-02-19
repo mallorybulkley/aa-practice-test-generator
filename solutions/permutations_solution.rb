0f2f08b5211a547cfc452e62225cdede0fa9180f
Simcha Cohen <simcha_cohen@yahoo.com>
2019-02-19 17:04:32 -0500

    permutations

--- solutions/permutations_solution.rb
+++ solutions/permutations_solution.rb
@@ -5,7 +5,7 @@ def permutations(array)
   # Similar to the subsets problem, we observe that to get the permutations
   # of [1, 2, 3] we can look at the permutations of [1, 2] which are
   # [1, 2] and [2, 1] and add the last element to every possible index getting
-  # [3, 1, 2], [1, 3, 2], [1, 2, 3], [3, 2, 1], [2, 3, 1]
+  # [3, 1, 2], [1, 3, 2], [1, 2, 3], [3, 2, 1], [2, 3, 1], [2, 1, 3]
 
   # pop off the last element
   first = array.shift
@@ -20,8 +20,8 @@ def permutations(array)
   # gets added to total_permutations.
   perms.each do |perm|
     (0..perm.length).each do |i|
-      total_permutations << perm[0 ... i] + [first] + perm[i .. -1]
+      total_permutations << perm[0...i] + [first] + perm[i..-1]
     end
   end
-  total_permutations.sort
+  total_permutations
 end
