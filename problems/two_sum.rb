f3f44b02fab6a079d6faef91a755301878d0b8b6
Simcha Cohen <simcha_cohen@yahoo.com>
2019-02-20 12:11:32 -0500

    two_sum

--- problems/two_sum.rb
+++ problems/two_sum.rb
@@ -1,9 +1,10 @@
 class Array
   # Write a method, `Array#two_sum`, that finds all pairs of positions where the
-  # elements at those positions sum to zero.
+  # elements at those positions sum to zero. The method should return a nested 
+  # array of positions.
 
-  # NB: ordering matters. I want each of the pairs to be sorted smaller index
-  # before bigger index. I want the array of pairs to be sorted
+  # NB: ordering matters. We want each of the pairs to be sorted smaller index
+  # before bigger index. We want the array of pairs to be sorted
   # "dictionary-wise":
   #   [0, 2] before [1, 2] (smaller first elements come first)
   #   [0, 1] before [0, 2] (then smaller second elements come first)
--- solutions/two_sum_solution.rb
+++ solutions/two_sum_solution.rb
@@ -2,8 +2,9 @@
 class Array
   def two_sum
     pairs = []
-    (0...length).each do |i|
-      ((i + 1)...length).each do |j|
+    
+    self.each_index do |i|
+      ((i + 1)...self.length).each do |j|
         pairs << [i, j] if self[i] + self[j] == 0
       end
     end
