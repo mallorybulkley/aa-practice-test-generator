9d46b53ebf05cd2697b783a203521a2df934763c
Simcha Cohen <simcha_cohen@yahoo.com>
2019-02-28 16:41:13 -0500

    my_reject_specs

--- specs/my_reject_spec.rb
+++ specs/my_reject_spec.rb
@@ -1,13 +1,13 @@
 describe 'my_reject' do
 
-  a = [1, 2, 3]
+  let(:arr) { [1, 2, 3] }
 
   it 'It correctly selects elements that do not match the passed in block' do
-    expect(a.my_reject { |num| num > 1 }).to eq([1])
+    expect(arr.my_reject { |num| num > 1 }).to eq([1])
   end
 
   it 'It returns all elements if no elements match the block' do
-    expect(a.my_reject { |num| num == 4 }).to eq([1,2,3])
+    expect(arr.my_reject { |num| num == 4 }).to eq([1,2,3])
   end
 
 end
