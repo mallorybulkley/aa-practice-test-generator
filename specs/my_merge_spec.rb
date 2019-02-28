3c672a106349b7a3d3b2e0036ee32f1cbdb6e8dd
Simcha Cohen <simcha_cohen@yahoo.com>
2019-02-28 17:30:55 -0500

    my_merge_specs

--- specs/my_merge_spec.rb
+++ specs/my_merge_spec.rb
@@ -1,7 +1,7 @@
 describe "my_merge" do
-  let(:arr1){ {a: 1, b: 2, c: 3} }
-  let(:arr2){ {d: 4, e: 5} }
-  let(:arr3){ {c: 33, d: 4, e: 5} }
+  let(:arr1) { {a: 1, b: 2, c: 3} }
+  let(:arr2) { {d: 4, e: 5} }
+  let(:arr3) { {c: 33, d: 4, e: 5} }
 
   it "Merges 2 hashes and returns a hash" do
       expect(arr1.my_merge(arr2)).to eq(arr1.merge(arr2))
