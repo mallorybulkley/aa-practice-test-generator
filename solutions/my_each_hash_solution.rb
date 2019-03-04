c142cc784c7b6c69ded888b75410331c6900d476
Michael Schwartz <MichaelSchwar3@gmail.com>
2019-03-04 10:24:59 -0500

    Fix my each hash

--- solutions/my_each_hash_solution.rb
+++ solutions/my_each_hash_solution.rb
@@ -1,16 +1,17 @@
 class Hash
 
-  # def my_each(&prc)
-  #   keys.each do |k|
-  #     prc.call(k, self[k])
-  #   end
-  # end
-
   def my_each(&prc)
-    k = 0
-    while k < keys.length
-      prc.call(keys[k], self[k])
+    keys.each do |k|
+      prc.call(k, self[k])
     end
   end
+
+  # def my_each(&prc)
+  #   k = 0
+  #   while k < keys.length
+  #     prc.call(keys[k], self[keys[k]])
+  #     k+=1
+  #   end
+  # end
 
 end
