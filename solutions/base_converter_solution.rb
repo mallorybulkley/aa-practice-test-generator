621d6b4d1fcf3ea47433e8a89429930b36417e69
ronilbhatia <ronilbhatia@gmail.com>
2019-03-06 15:10:48 -0800

    Make base converter a bonus

--- solutions/base_converter_solution.rb
+++ solutions/base_converter_solution.rb
@@ -1,13 +1,6 @@
 def base_converter(num, b)
-  return "0" if num == 0
+  return num.to_s if [0,1].include?(num)
 
   digits = %w(0 1 2 3 4 5 6 7 8 9 a b c d e f)
-  result = ""
-
-  until num == 0
-    result.insert(0, digits[num % b])
-    num /= b 
-  end
-
-  result
+  base_converter(num/b, b) + digits[num % b]
 end
