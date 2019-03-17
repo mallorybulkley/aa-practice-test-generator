47af3d45d73d0876ef793294fc0099ca2cea7311
Ronil Bhatia <ronilbhatia@gmail.com>
2019-03-16 20:23:16 -0700

    Fix my_select spec and solution

--- solutions/my_select_solution.rb
+++ solutions/my_select_solution.rb
@@ -2,7 +2,7 @@ class Array
   def my_select(&prc)
     selects = []
 
-    self.my_each do |item|
+    self.each do |item|
       selects << item if prc.call(item)
     end
 
--- specs/my_select_spec.rb
+++ specs/my_select_spec.rb
@@ -2,11 +2,9 @@ describe 'my_select' do
 
   let(:arr) { [1, 2, 3] }
 
-  it "should use #my_each, NOT built-in ruby method" do
-    expect(arr).to receive(:my_each).and_call_original
-    expect(arr).not_to receive(:each)
-    expect(arr).not_to receive(:map)
-    expect(arr).not_to receive(:map!)
+  it "should NOT use built-in #select method" do
+    expect(arr).not_to receive(:select)
+    expect(arr).not_to receive(:select!)
     arr.my_select { |num| num > 1 }
   end
 
