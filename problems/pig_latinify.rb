52caf34bbb36e983019f68029b0b8ec25591aa83
Ronil Bhatia <ronilbhatia@gmail.com>
2019-03-17 20:24:17 -0700

    Add pig latin transalation rules to pig_latinify problem

--- problems/pig_latinify.rb
+++ problems/pig_latinify.rb
@@ -1,6 +1,15 @@
 # Write a method that translates a sentence into pig latin. You may want a helper method.
+
+## Rules
+# Pig latin translation uses the following rules:
+# - for words that start with a vowel, add 'ay' to the end
+# - for words that start with a nonvowel, move all letters before the first vowel to the end of the word and add 'ay'
+# - 'qu' counts as a consonant (see third example)
+
+## Examples
 # 'apple' => 'appleay'
 # 'pearl' => 'earlpay'
 # 'quick' => 'ickquay'
+
 def pig_latinify(sentence)
 end
