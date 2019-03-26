761286a4e2e847e9baccff9624c9be59472e6885
ronilbhatia <ronilbhatia@gmail.com>
2019-03-26 11:38:37 -0700

    Fix typos in anagrams problem

--- problems/anagrams.rb
+++ problems/anagrams.rb
@@ -1,8 +1,7 @@
-# Write a method anagrams? that takes in two words and returns a boolean 
+# Write a function anagrams(str1, str2) that takes in two words and returns a boolean
 # indicating whether or not the words are anagrams. Anagrams are words that
-# contain the same characters but not necessarily in the same order. Solve this 
-# without using .sort
-
+# contain the same characters but not necessarily in the same order. Solve this
+# without using Array#sort
 def anagrams(str1, str2)
   
 end
