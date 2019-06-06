88585b065936e15ac00e2c44866de4b03a5fa3ae
ronilbhatia <ronilbhatia@gmail.com>
2019-06-06 16:42:45 -0700

    Add clearer description for pig_latinify

--- problems/pig_latinify.rb
+++ problems/pig_latinify.rb
@@ -1,15 +1,22 @@
+# **THIS PROBLEM WILL NOT SHOW UP ON A REAL ASSESSMENT** 
+# If you are a non-native English speaker and find it difficult to understand this
+# problem do not spend too much time on it and focus on other problems instead.
+
 # Write a method that translates a sentence into pig latin. You may want a helper method.
 
 ## Rules
+# In the English language, vowels are the following letters: ['a', 'e', 'i', 'o', 'u'] 
+# Consonants are all other letters.
 # Pig latin translation uses the following rules:
-# - for words that start with a vowel, add 'ay' to the end
-# - for words that start with a nonvowel, move all letters before the first vowel to the end of the word and add 'ay'
-# - 'qu' counts as a consonant (see third example)
-
-## Examples
-# 'apple' => 'appleay'
-# 'pearl' => 'earlpay'
-# 'quick' => 'ickquay'
+#  1. If the word begins with a vowel, simply append `ay` onto the end.
+#      ex. 'apple' => 'appleay'
+#  2. If the word begins with a consonant, move the starting consonants to the
+#  end of the word and then append `ay`
+#      ex1. 'pearl' => 'earlpay'
+#      ex2. `school` => `oolschay`
+#  3. Treat `qu` at the start of a word as a singular consonant.
+#      ex1. `queen` => `eenquay`
+#      ex2. `square` => `aresquay`
 def pig_latinify(sentence)
   
 end
